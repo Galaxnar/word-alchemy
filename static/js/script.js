777b0a0ec0354d55598696b11afded44f2e9fd50
Etienne <etienne.leclerc@polymtl.ca>
2024-02-08 13:46:26 -0500

    added xmlhttprequest in js

--- static/js/script.js
+++ static/js/script.js
@@ -15,3 +15,26 @@ function displayText() {
         displayArea.appendChild(p);
     });
 }
+
+function checkFileExistence() {
+    var textInput = document.getElementById("textInput").value;
+    var filePath = 'https://raw.githubusercontent.com/Galaxnar/word-alchemy/main/synonymes/Afrique';
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', filePath, true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status == 200) {
+          console.log('File exists.');
+          console.log('File content:', xhr.responseText);
+          var p = document.createElement("p");
+          p.textContent = 'YES';
+          displayArea.appendChild(p);
+        } else {
+          console.log('File does not exist or cannot be accessed.');
+        }
+      }
+    };
+    xhr.send();
+    
+  }
